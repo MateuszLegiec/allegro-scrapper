0cfe56edd182941943f6d88e2139ad00b2f656a8
Mateusz Legiec <legiec.mateusz@gmail.com>
2020-12-09 17:47:15 +0100

    Get html from allegro

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,29 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.URL
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val content = readContent(getURL("gra planszowa"))
+
+    print(content)
+}
+
+fun readContent(baseURL: String): String {
+    val url = URL(baseURL)
+    val connection = url.openConnection()
+    val bufferedReader = BufferedReader(InputStreamReader(connection.getInputStream()))
+    var inputLine = bufferedReader.readLine()
+    val stringBuilder = StringBuilder()
+
+    while (inputLine != null){
+        stringBuilder.append(inputLine)
+        inputLine = bufferedReader.readLine()
+    }
+    return stringBuilder.toString()
+}
+
+fun getURL(queryParam: String): String {
+    return "https://allegro.pl/listing?string=" + queryParam.replace(' ', '+')
 }
